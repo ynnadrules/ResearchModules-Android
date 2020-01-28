2b07de89ac1b2ea3218a1c726aef2743b178746c
Danny Ruiz <ynnadrules@gmail.com>
2020-01-28 15:55:02 -0600

    Integrate FlankerKit. Bridging coming soon

--- flanker-test-app/src/main/swift/Sources/FlankerKitCore/FlankerKitCore.swift
+++ flanker-test-app/src/main/swift/Sources/FlankerKitCore/FlankerKitCore.swift
@@ -1,15 +1,34 @@
 import java_swift
 import Foundation
 import AnyCodable
+import FlankerKit
 
 public class FlankerKitCore {
   public static func bootstrap(cachePath: String?) {
     setenv("TMPDIR", cachePath!, 1)
     // Required for SSL to work
     setenv("URLSessionCertificateAuthorityInfoFile", cachePath! + "/cacert.pem", 1)
+
+    let persistence = FlankerLocalPersistentData(startDate: .init())
+    let engine = FlankerEngine(with: [TestStep()], localPersistentData: persistence)
+
+    NSLog("\(engine)")
   }
 
   public static func diagnostics() -> String {
     return Bundle.main.bundlePath
   }
+}
+
+struct TestStep: FlankerStepProtocol {
+  var nextStepIdentifier: String? = nil
+
+  var branchingNavigationRules: [FlankerStepBranchingRule]? = nil
+
+  var stepGroup: FlankerStepGroup? = nil
+
+  var stepBackTo: String? = nil
+
+  let identifier = UUID().uuidString
+  var stepName: String? = "Test"
 }
